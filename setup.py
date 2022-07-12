882347efb5f95292276245aa552868b985bc79f7
codeif <me@codeif.com>
2022-07-12 20:33:03 +0800

    version: 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='curl2py',
-    version='0.0.2',
+    version='0.0.3',
     description='Conver cURL command line to python-requests code.',
     author='codeif',
     author_email='me@codeif.com',
